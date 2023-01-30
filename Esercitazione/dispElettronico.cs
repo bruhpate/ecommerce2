c95ccc7a72c135912d6d1336f5c3316911fa85c9
bruhpate <pietropatelli05@gmail.com>
2023-01-30 10:05:52 +0100

    commit sotto ordine del bernini

---
+++ Esercitazione/dispElettronico.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione
+{
+    public class DispElettronico : prodotto
+    {
+        private bool _lunedì = false;
+        private string _modello;
+        private int _sconto;
+        public string Modello { get { return _modello; } set { _modello = value; }}
+        public override float Prezzo
+        {
+            get
+            {
+                if (_lunedì)
+                {
+                    return this.Prezzo / 100 * 95;
+                }
+                return this.Prezzo;
+            }
+        }
+        public int Sconto
+        {
+            set { _sconto = value; }
+            get { return _sconto; }
+        }
+        public DispElettronico(string id, string nome, string produttore, string descrizione, float prezzo, string modellospec) : base(id, nome, produttore, descrizione, prezzo)
+        {
+            Modello = modellospec;
+        }
+
+    }
+}
---
+++ Esercitazione/prodotto.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercitazione
+{
+    public class prodotto
+    {
+        private string _id, _nome, _produttore, _descrizione;
+        private float _prezzo;
+
+        public prodotto()
+        {
+
+        }
+        public prodotto(string id, string nome, string produttore, string descrizione, float prezzo)
+        {
+            _id = id;
+            _nome = nome;
+            _produttore = produttore;
+            _descrizione = descrizione;
+            _prezzo = prezzo;
+        }
+
+        public string Id
+        {
+            set { _id = value; }
+            get { return _id; }
+        }
+        public string Nome
+        {
+            get { return _nome; }
+        }
+        public string Produttore
+        {
+            get { return _produttore; }
+        }
+        public string Descrizione
+        {
+            get { return _descrizione; }
+        }
+
+        public virtual float Prezzo
+        {
+            get { return _prezzo; }
+        }
+    }
+}
